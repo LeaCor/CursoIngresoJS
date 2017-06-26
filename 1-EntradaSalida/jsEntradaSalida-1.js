fadb7dde17b24c3b6484420e0b0d0e11e11d9a9c
LeaCor <leacor03@gmail.com>
2017-06-26 12:26:38 -0300

    1ra Clase 26/6/2017

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,12 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostar()
 {
-	
+	var mensaje="Esto funciona de maravilla";
+    alert(mensaje);
+    /*Hay otra opcion mas directa
+     
+     alert("Esto funciona de maravilla");
+    
+    */
 }
 
--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,12 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
-function Mostar()
-{
-	
+function Mostrar()
+{   var mensaje;
+    mensaje=prompt("El color de las olas turbias es");//El dato que se introduce se guarda en la variable mensaje
+    //es decir prompt se guarda en mensaje
+    alert(mensaje);
+    //Se puede escribir de manera simplificada 
+    //alert(prompt("El color de las olas turbias es"));
+    
 }
 
