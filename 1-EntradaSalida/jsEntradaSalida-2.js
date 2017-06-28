6dd23acba95a459a8e87e9368bb57c7484bc4614
LeaCor <leacor03@gmail.com>
2017-06-27 23:08:02 -0300

    Ejercicios realizados hasta el numero 6 exceptuando el numero 4

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,8 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
-function Mostrar()
-{   var mensaje;
+function Mostar()
+{   
+    var mensaje
     mensaje=prompt("El color de las olas turbias es");//El dato que se introduce se guarda en la variable mensaje
     //es decir prompt se guarda en mensaje
     alert(mensaje);
--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,16 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
+var a;
+var b;
+a=document.getElementById("numeroUno").value;
 
+b=document.getElementById("numeroDos").value;
+
+a=parseInt(a);
+b=parseInt(b);
+c=a+b;
+
+alert("El resultado es "+c);
 }
 
