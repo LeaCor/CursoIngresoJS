7bdf3533347d29c4c728d3dd4b6b72afa5869015
LeaCor <leacor03@gmail.com>
2017-07-02 16:26:37 -0300

    Hasta el ejercicio 7 IF

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,27 @@
 function Mostrar()
 {
 //tomo la edad  
+    var edad;
 
+    edad=document.getElementById("edad").value;
 
+    edad=parseInt(edad);
 
+    if(edad>=18)
 
+    {
+        alert("Eres mayor de edad")
+    }
+
+    else{
+        if(edad>=13&&edad<=17)
+        {
+            alert("Eres adolescente")
+        }
+        else
+        {
+            alert("Eres un niño")
+        }
+    }
+    
 }//FIN DE LA FUNCIÓN
