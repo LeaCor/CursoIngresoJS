275530311e14efc7f4daf156a0de5f37e061f591
LeaCor <leacor03@gmail.com>
2017-07-02 18:25:43 -0300

    hASTA EL if n°8

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,29 @@
 function Mostrar()
 {
-//tomo la edad  
+    var edad;
+    var estadoCivil;
 
-	
+edad=document.getElementById("edad").value;
+estadoCivil=document.getElementById("estadoCivil").value;
+//aca se toma los tre valores posibles SOLTERO;CASADO;DIVORCIADO
+
+edad=parseInt(edad);
 
 
+var soltero=(estadoCivil=="Soltero");
+var noSoltero=(estadoCivil!="Soltero");
+
+//EN EL 'IF' PUEDO USAR ESTAS NUEVAS VARIABLES O LA VARIABLE 'ESTADOCIVIL'Y SUS COMPARACIONES
+
+    if(edad<18&&noSoltero)
+    {
+
+    }
+    else
+    {
+        if(edad>=18&&soltero)
+        {
+            alert("Es soltero y no es menor");
+        }
+    }
 }//FIN DE LA FUNCIÓN
