4de0708f4e51dbce87dfb51a62386b26df4bb03a
LeaCor <leacor03@gmail.com>
2017-07-03 12:17:02 -0300

    Hasta Switch n 6

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -3,7 +3,15 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+switch(mesDelAño)
+{
+    case "Febrero":
+        alert("Este mes no tiene 29");
+        break;
+    default:
+        alert("Este mes tiene 30 a mas dias");
+        break;
+}        
 	
 	
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -3,6 +3,29 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+switch(mesDelAño)
+{
+    case "Febrero":
+        alert("Tiene 28 dias");
+        break;
+    
+    case "Enero":
+    case "Marzo":
+    case "Mayo":
+    case "Julio":
+    case "Agosto":
+    case "Octubre":
+    case "Diciembre":
+        alert("Tiene 31 dias");
+        break;
+
+    case "Abril"://tambien con 'default'
+    case "Junio":
+    case "Septiembre":
+    case "Noviembre":
+        alert("Tiene 30 dias");
+        break;
+}
 //alert (mesDelAño);
 	
 	
