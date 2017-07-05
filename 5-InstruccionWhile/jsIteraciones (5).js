415b5ec77a2de0b91128235d11dda4f93e200555
LeaCor <leacor03@gmail.com>
2017-07-04 22:58:26 -0300

    Iteracion hasta n 5

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -3,6 +3,12 @@ function Mostrar()
 
 var sexo = prompt("ingrese f ó m .");
 
+while(sexo!="f" && sexo!="m")// uso && porque son dos valores String 
+//mientras no sea ni "f" ni "m" va ser TRUE y se ejecuta el bucle
+//no va con || porque no es un rango como en el caso anterior
+{
+    sexo=prompt("ingrese f ó m.");
+}
 
 
 document.getElementById('Sexo').value=sexo;
