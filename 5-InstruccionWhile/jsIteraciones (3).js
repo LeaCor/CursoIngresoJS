da22e5eca34b2c5a58f32f1e2eb92ea7af00678e
LeaCor <leacor03@gmail.com>
2017-07-05 11:56:32 -0300

    Comentario agregado y terminado

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -2,7 +2,8 @@ function Mostrar()
 { 
 
 
-    var clave = prompt("ingrese el número clave.");
+    var clave = prompt("ingrese el número clave.");//tambien puedo dar valor cualquiera a clave...var clave="iuryeiur"
+    //lo haria de esta otra manera para ingresar al while.
     while(clave!="utn750")
     {
         clave=prompt("Ingrese el número de clave");
