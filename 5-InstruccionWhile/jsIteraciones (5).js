b200cae892b6e1d3405ff34e567054575bb23f53
LeaCor <leacor03@gmail.com>
2017-07-06 16:29:58 -0300

    Comentado y terminado

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -1,14 +1,17 @@
 function Mostrar()
 {
 
-var sexo = prompt("ingrese f ó m .");
+    var sexo = prompt("ingrese f ó m .");
 
-while(sexo!="f" && sexo!="m")// uso && porque son dos valores String 
-//mientras no sea ni "f" ni "m" va ser TRUE y se ejecuta el bucle
-//no va con || porque no es un rango como en el caso anterior
-{
-    sexo=prompt("ingrese f ó m.");
-}
+    while(sexo!="f" && sexo!="m")// uso && porque son dos valores String 
+    //mientras no sea ni "f" ni "m" va ser TRUE y se ejecuta el bucle
+    //SE PUEDE AGREGAR "sexo!="F" && sexo!="M" (Mayusculas)
+    //OTRA FORMA:   
+    //WHILE (!(sexo=="f"||sexo=="m"));
+    //Si ingreso una caracter diferente es False y luego TRUE por el signo de admiracion "!"
+    {
+        sexo=prompt("ingrese f ó m.");
+    }
 
 
 document.getElementById('Sexo').value=sexo;
