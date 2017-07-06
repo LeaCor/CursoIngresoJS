34acad3767a39c970d20323a9d3cc4053436088d
LeaCor <leacor03@gmail.com>
2017-07-06 16:31:36 -0300

    Comentado y Terminado

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,7 +3,17 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero;
+	while(contador<5)
+	{
+		numero=parseInt(prompt("Ingresa numero:"));
+		
+		acumulador=numero+acumulador
+		
+		contador++
+		
 
+	}
 
 
 document.getElementById('suma').value=acumulador;
